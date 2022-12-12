022727ef310eb00606de646d26e3eef278fe7514
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-12-11 23:10:31 -0500

    small fixes

--- options.js
+++ options.js
@@ -16,7 +16,6 @@ $(".spectrum").spectrum({
     allowEmpty:true,
     showPalette: true,
     preferredFormat: "hex",
-    color: "#009933",
     palette: [
         ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
         ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
